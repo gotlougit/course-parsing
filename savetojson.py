6e1da0b76c8854d3f78cb631764509ee063c4afe
Saksham Mittal <gotlouemail@gmail.com>
2022-02-01 21:40:06 +0530

    Streamline script by removing unnecessary functions

--- savetojson.py
+++ savetojson.py
@@ -1,8 +1,5 @@
 import sys, json
 
-def resetEntry():
-    return {"course code": "", "course title": "", "prerequisites": "", "course type": "", "course learning objectives":"", "course content":"", "course outcomes":"", "credits":"", "books": ""}
-
 def writeLines(lines, entry, key, start):
    
     i = start
@@ -31,15 +28,13 @@ def processVal(val):
     val = val.title()
     return val
 
-def preprocess(line, term):
+def isextraLine(line):
     
-    if parsingTerms.index(term) >= 5:
-        line = line.replace(term, "\n" + term + "\n")
-
-    if "books" in line and "reference" not in line:
-        line = line.replace("books","reference books")
-
-    return line
+    out = line != "\n"
+    out = out or line != "---"
+    out = out or "B. Tech" in line or "Semester" in line
+    out = out or "(2017-18 onwards)" in line
+    return out
 
 def write2dict(lines, entry, lline, term, lineno):
     
@@ -49,60 +44,41 @@ def write2dict(lines, entry, lline, term, lineno):
         lline = lline.replace(term.replace(" ",""),"")
     else:
         lline = x
+    #put all multiline entries AFTER course content so this works
     if (parsingTerms.index(term) >= 5):
         lineno = writeLines(lines, entry, term, lineno + 1)
         return lineno
     else:
         entry[term] = processVal(lline)
-    return 0
-
+        return 0
 
 fname = sys.argv[1]
 parsingTerms = ["course code", "course title", "prerequisites", "credits", "course type", "course content", "course learning objectives",  "course outcomes", "books"]
-entry = resetEntry()
+entry = {"course code": "", "course title": "", "prerequisites": "", "course type": "", "course learning objectives":"", "course content":"", "course outcomes":"", "credits":"", "books": ""}
 outname = fname + ".json"
 output = open(outname,"w")
 
 with open(fname) as f:
     lines = list(f.readlines())
-
-    lineno = 0
-    iters = 1 
     nlines = []
     for l in lines:
-        if l != "\n" or "---" not in l:
+        if isextraLine(l):
             l = l.lower()
             for term in parsingTerms:
-                if term in l:
-                    l = preprocess(l, term)
-                else:
-                    orig = term
-                    term = term.replace(" ","")
-                    if term in l:
-                        l = preprocess(l, orig)
+                if term in l or term.replace(" ","") in l:
+                    l = (l.replace(term, "\n" + term + "\n") if parsingTerms.index(term) >= 5 else l)
             nlines.append(l)
-         
     lines = nlines
-    n = len(lines) 
-    while lineno < n:
-        flag = 0
-        lline = lines[lineno]
+    lineno = 0
+    iters = 1 
+    while lineno < len(lines):
+        line = lines[lineno]
         for term in parsingTerms:
-            if term in lline:
-                flag = 1
-                x = write2dict(lines, entry, lline, term, lineno)
+            if term in line or term.replace(" ","") in line:
+                x = write2dict(lines, entry, line, term, lineno)
                 if x:
                     lineno = x
-                    continue
-            else:
-                orig = term
-                term = term.replace(" ","")
-                if term in lline:
-                    flag = 1
-                    x = write2dict(lines, entry, lline, orig, lineno)
-                    if x:
-                        lineno = x
-                        continue
+
         if not iters % 9:
             try:
                 entry["credits"] = (entry["credits"].replace("0",""))[0]
@@ -110,7 +86,7 @@ with open(fname) as f:
                 entry["credits"] = "0"
             output.write(json.dumps(entry, indent = 2))
             output.write("\n")
-            entry = resetEntry()
+            entry = entry.fromkeys(entry,"")
             iters = 0
 
         lineno += 1
